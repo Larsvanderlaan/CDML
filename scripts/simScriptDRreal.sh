7201deb7813bbcf112d940fc24a6569bd0155542
Lars van der Laan <vanderlaanlars@yahoo.com>
2023-08-30 15:22:43 -0700

    hey

--- scripts/simScriptDRreal.sh
+++ scripts/simScriptDRreal.sh
@@ -1,7 +1,7 @@
 #!/bin/usr/env bash
 export R_LIBS=~/Rlibs2
 export R_LIBS_USER=~/Rlibs2
-for data_name in "acic2017_19" "acic2017_20" "acic2017_23" "acic2017_24" "ihdp" "lalonde_cps" "lalonde_psid" "twins"
+for data_name in "ihdp" "lalonde_cps" "lalonde_psid" "twins" "acic2017_19" "acic2017_20" "acic2017_23" "acic2017_24"
 do
   sbatch  --export=data_name=$data_name ~/DRinference/scripts/simScriptDRreal.sbatch
 done
