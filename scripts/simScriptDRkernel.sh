ee84e1e5e72388ffc196c00e6aa5610f292fad68
Lars van der Laan <vanderlaanlars@yahoo.com>
2023-09-15 15:40:26 -0700

    hey

--- scripts/simScriptDRkernel.sh
+++ scripts/simScriptDRkernel.sh
@@ -2,7 +2,7 @@
 nsims=2500
 export R_LIBS=~/Rlibs2
 export R_LIBS_USER=~/Rlibs2
-for n in 100 250 500 750 1000 3000 5000 9000
+for n in 100 250 500 750 1000 2000 3000 4000 5000 7500 9000
 do
   for const in 2
       do
