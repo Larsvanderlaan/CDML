cbf7ccf353aea55eab5faa83b5016917ddd66f74
Lars van der Laan <vanderlaanlars@yahoo.com>
2023-09-15 15:27:15 -0700

    hey

---
+++ scripts/DRkernel.R
@@ -0,0 +1,306 @@
+library(data.table)
+library(SuperLearner)
+library(sl3)
+if(!require(drtmle)) {
+  devtools::install_cran("np")
+  devtools::install_github("benkeser/drtmle")
+}
+library(xgboost)
+
+d <- 2
+seed_start <- 98103
+set.seed(seed_start)
+
+SL.kernelcustom <- function (Y, X, newX, family = gaussian(), obsWeights = rep(1,
+                                                                               length(Y)), rangeThresh = 1e-07, ...)
+{
+  options(np.messages = TRUE)
+
+
+
+  bw <- np::npregbw(xdat = X, ydat = Y  )
+
+  print("hi")
+  print(bw$bw)
+  thisMod <- np::npreg(bw, txdat = X, tydat = Y, exdat = newX)
+
+   fit <- list(object = thisMod, txdat = X, tydat = Y, bw = bw)
+  class(fit) <- "SL.kernelcustom"
+  out <- list(pred = thisMod$mean, fit = fit)
+  return(out)
+}
+
+predict.SL.kernelcustom <- function (object, newdata, ...)
+{
+
+  if (is.matrix(newdata)) {
+    newdata = as.data.frame(newdata)
+  }
+  pred <- np::npreg(object$bw, txdat = object$txdat,  tydat= object$tydat,  exdat = newdata)$mean
+
+  #pred <- np:::predict.npregression(object = object$object, newdata = newdata
+   #            )
+
+
+
+  return(pred)
+}
+
+
+do_sims <- function(n, nsims) {
+
+
+
+  lrnr_kernel <-   Lrnr_pkg_SuperLearner$new("SL.kernelcustom")
+  lrnr_kernel <- Lrnr_cv$new(lrnr_kernel)
+
+
+  sim_results <- lapply(1:nsims, function(i){
+    try({
+      set.seed(seed_start + i)
+      print(paste0("iter: ", i))
+      data_list <- get_data(n)
+      W <- data_list$W
+      A <- data_list$A
+      Y <- data_list$Y
+      ATE <- data_list$ATE
+      n <- length(A)
+      nfolds <- 10
+      initial_estimators_kernel <- compute_initial(W,A,Y, lrnr_mu = lrnr_kernel, lrnr_pi = lrnr_kernel, folds = nfolds, stratify_trt = TRUE)
+      folds <- initial_estimators_kernel$folds
+      initial_estimators_misp <- compute_initial(W,A,Y, lrnr_mu =  Lrnr_cv$new(Lrnr_glm$new()), lrnr_pi =  Lrnr_cv$new(Lrnr_glm$new()), folds = folds, stratify_trt = FALSE)
+
+      mean(initial_estimators_kernel$mu1 - initial_estimators_kernel$mu0)
+      mean((A - initial_estimators_kernel$pi1)/ (initial_estimators_kernel$pi1 * (1-initial_estimators_kernel$pi1)) * Y)
+
+      out_list <- list()
+
+      for(lrnr in c("kernel")) {
+        initial_estimators <- initial_estimators_kernel
+        for(misp in c("1", "2", "3" )) {
+          mu1 <- initial_estimators$mu1
+          mu0 <- initial_estimators$mu0
+          pi1 <- initial_estimators$pi1
+          pi0 <- initial_estimators$pi0
+          if(misp == "2") {
+            mu1 <- initial_estimators_misp$mu1
+            mu0 <- initial_estimators_misp$mu0
+          } else if(misp == "3" ) {
+            pi1 <- initial_estimators_misp$pi1
+            pi0 <- initial_estimators_misp$pi0
+          } else if(misp == "4") {
+            mu1 <- initial_estimators_misp$mu1
+            mu0 <- initial_estimators_misp$mu0
+            pi1 <- initial_estimators_misp$pi1
+            pi0 <- initial_estimators_misp$pi0
+          }
+
+          print(mean(mu1 - mu0))
+          out_AIPW <- compute_AIPW(A,Y, mu1=mu1, mu0 =mu0, pi1 = pi1, pi0 = pi0)
+          out_AuDRIE <- compute_AuDRIE_boot(A,Y,  mu1=mu1, mu0 =mu0, pi1 = pi1, pi0 = pi0, nboot = 500, folds = folds, alpha = 0.05)
+          out_drtmle <- compute_drtmle(W, A,Y, mu1=mu1, mu0 =mu0, pi1 = pi1, pi0 = pi0, folds = nfolds)
+
+
+          out <- as.data.table(rbind(
+            rbind(unlist(out_AuDRIE), unlist(out_AIPW)),
+            unlist(out_drtmle)
+            ))
+          colnames(out) <- c("estimate", "CI_left", "CI_right")
+          out$misp <- misp
+          out$estimator <- c("auDRI", "AIPW", "drtmle")
+          out$lrnr <- lrnr
+          out_list[[paste0(misp, lrnr)]] <- out
+        }
+      }
+      out <- rbindlist(out_list)
+      out$n <- n
+      out$ATE <- ATE
+      out$iter <- i
+      return(as.data.table(out))
+    })
+    return(data.table())
+  })
+  sim_results <- data.table::rbindlist(sim_results)
+  key <- paste0("DR_iter=", nsims, "_n=", n )
+  try({fwrite(sim_results, paste0("~/DRinference/simResultsDR/sim_results_", key, ".csv"))})
+  return(sim_results)
+}
+
+
+get_data <- function(n, ...) {
+
+  W1 <- runif(n, -2 , 2)
+  W2 <- rbinom(n, 1, 0.5)
+  W <- cbind(W1, W2)
+  colnames(W) <- c("W1", "W2")
+  pi0 <- plogis(-W1 + 2 * W1 * W2)
+  A <- rbinom(n, 1, pi0)
+  mu1 <- plogis(0.2*1 - W1 + 2*W1*W2)
+  mu0 <- plogis(0.2*0 - W1 + 2*W1*W2)
+  Y <- rbinom(n, 1,ifelse(A==1, mu1, mu0))
+  ATE <- 0.03803004
+
+  out <- list(W=W, A = A, Y = Y,  pi0 = pi0, mu1 = mu1, mu0 = mu0, ATE = ATE)
+
+  return(out)
+}
+
+
+
+
+
+compute_AuDRIE_boot <-  function(A,Y, mu1, mu0, pi1, pi0, nboot = 1000, folds, alpha = 0.05) {
+  data <- data.table(A, Y, mu1, mu0, pi1, pi0)
+  folds <- lapply(folds, `[[`, "validation_set")
+  tau_n <- compute_AuDRIE(A,Y, mu1, mu0, pi1, pi0)
+
+  bootstrap_estimates <- sapply(1:nboot, function(iter){
+    try({
+      bootstrap_indices <- unlist(lapply(folds, function(fold) {
+        sample(fold, length(fold), replace = TRUE)
+      }))
+      data_boot <- data[bootstrap_indices,]
+      tau_boot <- compute_AuDRIE(data_boot$A, data_boot$Y, mu1 = data_boot$mu1, mu0 = data_boot$mu0, pi1 = data_boot$pi1, pi0 =data_boot$pi0)
+      return(tau_boot)
+    })
+    return(NULL)
+  })
+
+
+
+
+  CI <- tau_n + quantile(bootstrap_estimates - median(bootstrap_estimates), c(alpha/2, 1-alpha/2), na.rm = TRUE)
+  return(list(estimate = tau_n, CI = CI))
+}
+
+
+
+compute_AuDRIE <- function(A,Y, mu1, mu0, pi1, pi0) {
+  calibrated_estimators  <- calibrate_nuisances(A,Y, mu1 = mu1, mu0 = mu0, pi1 = pi1, pi0 = pi0)
+  mu1_star <- calibrated_estimators$mu1_star
+  mu0_star <- calibrated_estimators$mu0_star
+  pi1_star <- calibrated_estimators$pi1_star
+  pi0_star <- calibrated_estimators$pi0_star
+
+  mu_star <- ifelse(A==1, mu1_star, mu0_star)
+  alpha_n <- ifelse(A==1, 1/pi1_star, - 1/pi0_star)
+  tau_n <-  mean(mu1_star - mu0_star + alpha_n * (Y - mu_star))
+  return(tau_n)
+}
+
+compute_drtmle <- function(W, A,Y, mu1, mu0, pi1, pi0, ...) {
+  #Qn = cbind(mu0, mu1), gn = cbind(pi0, pi1)
+  # appears to be a bug if user supplied
+  out <- drtmle(W = W, A = A, Y = Y, , a_0 = c(0,1),
+                Qn = list(A0 = mu0, A1 = mu1), gn = list(A0 = pi0, A1= pi1),
+                SL_gr = "SL.kernelcustom",
+                SL_Qr = "SL.kernelcustom")$drtmle
+  tau_n <- out$est[2] -  out$est[1]
+  se <-  sqrt(as.vector(c(1, -1) %*% out$cov %*% c(1, -1)))
+  CI <- tau_n + c(-1,1) * qnorm(1-0.025) * se
+  return(list(estimate = tau_n, CI = CI))
+
+}
+
+compute_AIPW <- function(A,Y, mu1, mu0, pi1, pi0) {
+  n <- length(A)
+  pi1 <- pmax(pi1,  25/(sqrt(n)*log(n)))
+  pi0 <- pmax(pi0,  25/(sqrt(n)*log(n)))
+  mu <- ifelse(A==1, mu1, mu0)
+  alpha_n <- ifelse(A==1, 1/pi1, - 1/pi0)
+  tau_n <-  mean(mu1 - mu0 + alpha_n * (Y - mu))
+  CI <- tau_n + c(-1,1) * qnorm(1-0.025) * sd(mu1 - mu0 + alpha_n * (Y - mu))/sqrt(n)
+  return(list(estimate = tau_n, CI = CI))
+}
+
+
+isoreg_with_xgboost <- function(x,y,max_depth = 15, min_child_weight = 20) {
+  data <- xgboost::xgb.DMatrix(data = as.matrix(x), label = as.vector(y))
+  iso_fit <- xgboost::xgb.train(params = list(max_depth = max_depth,
+                                              min_child_weight = min_child_weight,
+                                              monotone_constraints = 1,
+                                              eta = 1, gamma = 0,
+                                              lambda = 0),
+                                data = data, nrounds = 1)
+  fun <- function(x) {
+    data_pred <- xgboost::xgb.DMatrix(data = as.matrix(x))
+    pred <- predict(iso_fit, data_pred)
+    return(pred)
+  }
+  return(fun)
+}
+
+calibrate_nuisances <- function(A, Y,mu1, mu0, pi1, pi0) {
+
+  calibrator_mu1 <- isoreg_with_xgboost(mu1[A==1], Y[A==1])
+  mu1_star <- calibrator_mu1(mu1)
+  calibrator_mu0 <- isoreg_with_xgboost(mu0[A==0], Y[A==0])
+  mu0_star <- calibrator_mu0(mu0)
+
+
+  calibrator_pi1 <- isoreg_with_xgboost(pi1, A)
+  pi1_star <- calibrator_pi1(pi1)
+  calibrator_pi0 <- isoreg_with_xgboost(pi0, 1-A)
+  pi0_star <- calibrator_pi0(pi0)
+  return(list(mu1_star= mu1_star, mu0_star=mu0_star, pi1_star = pi1_star, pi0_star = pi0_star))
+}
+
+
+compute_initial <- function(W,A,Y, lrnr_mu, lrnr_pi, folds, stratify_trt = TRUE,   invert = FALSE) {
+  data <- data.table(W,A,Y)
+  print(lrnr_mu)
+  if(stratify_trt) {
+    print("stratify")
+    taskY0 <- sl3_Task$new(data, covariates = colnames(W), outcome  = "Y", outcome_type = "binomial", folds = folds)
+    folds <- taskY0$folds
+    fit0 <- lrnr_mu$train(taskY0[A==0])
+
+    mu0 <- as.vector(fit0$predict(taskY0))
+
+    taskY1 <- sl3_Task$new(data, covariates = c(colnames(W)), outcome  = "Y", outcome_type = "binomial", folds = folds)
+    fit1 <- lrnr_mu$train(taskY1[A==1])
+    mu1 <-  as.vector(fit1$predict(taskY1))
+
+  } else {
+    taskY <- sl3_Task$new(data, covariates = c(colnames(W), "A"), outcome  = "Y", outcome_type = "binomial", folds = folds)
+    folds <- taskY$folds
+    fit <- lrnr_mu$train(taskY)
+    data1 <- data0 <- data
+    data1$A <- 1
+    data0$A <- 0
+    taskY1 <- sl3_Task$new(data1, covariates = c(colnames(W), "A"), outcome  = "Y", outcome_type = "binomial", folds = folds)
+    taskY0 <- sl3_Task$new(data0, covariates = c(colnames(W), "A"), outcome  = "Y", outcome_type = "binomial", folds = folds)
+    mu1 <-  as.vector(fit$predict(taskY1))
+    mu0 <-  as.vector(fit$predict(taskY0))
+  }
+
+
+  taskA <- sl3_Task$new(data, covariates = colnames(W), outcome  = "A", folds = folds, outcome_type = "binomial")
+
+  fit1 <- lrnr_pi$train(taskA)
+  pi1 <- as.vector(fit1$predict(taskA))
+  pi0 <- 1- pi1
+
+
+  print("done")
+  return(list(mu1 = mu1, mu0 = mu0, pi1 = pi1, pi0 = pi0, folds = folds))
+}
+
+truncate_pscore_adaptive <- function(A, pi, min_trunc_level = 1e-8) {
+  risk_function <- function(cutoff, level) {
+    pi <- pmax(pi, cutoff)
+    pi <- pmin(pi, 1 - cutoff)
+    alpha <- A/pi - (1-A)/(1-pi) #Riesz-representor
+    alpha1 <- 1/pi
+    alpha0 <- - 1/(1-pi)
+    mean(alpha^2 - 2*(alpha1 - alpha0))
+  }
+  cutoff <- optim(1e-5, fn = risk_function, method = "Brent", lower = min_trunc_level, upper = 0.5, level = 1)$par
+  pi <- pmin(pi, 1 - cutoff)
+  pi <- pmax(pi, cutoff)
+  pi
+}
+
+
+
+
---
+++ scripts/simScriptDRkernel.sh
@@ -0,0 +1,14 @@
+#!/bin/usr/env bash
+nsims=2500
+export R_LIBS=~/Rlibs2
+export R_LIBS_USER=~/Rlibs2
+for n in 100 250 500 750 1000 3000 5000 9000
+do
+  for const in 2
+      do
+    for misp in 1
+    do
+    sbatch  --export=n=$n,const=$const ~/DRinference/scripts/simScriptDRkernel.sbatch
+    done
+  done
+done
